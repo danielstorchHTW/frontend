f486ed874a488abf5e111793cfb36c7343a727d7
ChristopherDKeller <Christopher.Keller@Student.HTW-Berlin.de>
2023-06-20 12:07:49 +0200

    navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <nav>
-    <router-link to="/student">Student</router-link> |
-    <router-link to="/course">Course</router-link>   |
-    <router-link to="/grade">Grade</router-link>
-  </nav>
+  <div class="topnav">
+    <a class="active" href="/">Home</a>
+    <a href="student">Student</a>
+    <a href="course">Course</a>
+    <a href="grade">Grade</a>
+  </div>
+
   <router-view/>
 </template>
 
@@ -27,6 +29,34 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+/* Add a black background color to the top navigation */
+.topnav {
+  background-color: #333;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
 }
 </style>
 <script setup>
