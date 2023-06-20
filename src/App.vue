dc978f3e0680003c1dd2c5c40bacacc4a746d505
danielstorchHTW <Daniel.Storch@student.htw-berlin.de>
2023-06-20 12:49:40 +0200

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="topnav">
-    <a class="active" href="/">Home</a>
-    <a href="student">Student</a>
-    <a href="course">Course</a>
-    <a href="grade">Grade</a>
+    <a href="/" class="navvv">Home</a>
+    <a href="student" class="navvv">Student</a>
+    <a href="course" class="navvv">Course</a>
+    <a href="grade" class="navvv">Grade</a>
   </div>
 
   <router-view/>
@@ -17,40 +17,27 @@
   text-align: center;
   color: #2c3e50;
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-
 /* Add a black background color to the top navigation */
 .topnav {
-  background-color: #333;
+  background-color: #539f4b;
+  text-align: center;
   overflow: hidden;
 }
 
 /* Style the links inside the navigation bar */
 .topnav a {
-  float: left;
   color: #f2f2f2;
-  text-align: center;
-  padding: 14px 16px;
+  text-align: right;
+  padding: 14px 30px;
   text-decoration: none;
-  font-size: 17px;
+  font-size:20px;
 }
+
 
 /* Change the color of links on hover */
 .topnav a:hover {
-  background-color: #ddd;
-  color: black;
+  background-color: #063822;
+  color: #fafafa;
 }
 
 /* Add a color to the active/current link */
