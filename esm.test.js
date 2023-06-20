11808c0b6a28fd4af6de009af7d5fdb53bf1b8e9
danielstorchHTW <Daniel.Storch@student.htw-berlin.de>
2023-06-20 10:24:19 +0200

    delete

--- esm.test.js
+++
@@ -1,7 +0,0 @@
-import esm from "./esm";
-
-test("should know 1+1", () =>{
-    let expected = 2;
-    let actual = esm.add(1,1);
-    expect(actual).toBe(expected);
-})
--- first.test.js
+++
@@ -1,7 +0,0 @@
-const calcultor = require('./first');
-
-test('should add 1+1', () => {
-    let expected = 2;
-    let actual = calcultor.add(1,1);
-    expect(actual).toBe(actual);
-})
