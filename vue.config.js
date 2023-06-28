efed731fc6445bafc04c55fcf27153f9bc3b6f3b
ChristopherDKeller <Christopher.Keller@Student.HTW-Berlin.de>
2023-06-28 11:07:11 +0200

    .

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,5 @@ module.exports = {
   devServer: {
     port: '3000'
   },
-  publicPath: process.env.VUE_APP_BACKEND_BASE_URL === 'production'
-      ? '/frontend/'
-      : '/'
+  publicPath: '/frontend/'
 }
