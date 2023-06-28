643e257f6fe82fffaf6107aafeb5aed700f85914
ChristopherDKeller <Christopher.Keller@Student.HTW-Berlin.de>
2023-06-28 10:10:16 +0200

    test fix

--- tests/unit/dynamic-form.spec.js
+++ tests/unit/dynamic-form.spec.js
@@ -1,4 +1,4 @@
-import { shallowMount} from '@vue/test-utils'
+import {flushPromises, shallowMount} from '@vue/test-utils'
 import DynamicForm from '@/views/StudentView.vue'
 
 describe('dynamic form', () => {
@@ -6,17 +6,18 @@ describe('dynamic form', () => {
         fetch.resetMocks()
     })
 
-    it('should render the title passed to it', () => {
+    it('should render the title passed to it', async() => {
         fetch.mockResponseOnce(JSON.stringify([
             {id: 1222, name: 'Erwin', matrikelnr: 44444},
             {id: 1333, name: 'Carla', matrikelnr: 33333}
         ]))
 
-        const title = 'SaveStudent IDNameMatrikelnummerNo students yet'
+        const title = 'SaveStudent IDNameMatrikelnummerDelete1222Erwin44444Delete1333Carla33333'
         const wrapper = shallowMount(DynamicForm, {
             props: { title }
         })
         console.log(title)
+        await flushPromises()
         expect(wrapper.text()).toMatch(title)
     })
 
